0c2edc61ff3804da526bd618bb7c2a3811cf93da
MrAgungS <agung.worksss@gmail.com>
2026-02-27 21:44:52 +0700

    wsl crash

---
+++ backend-nest/src/auth/auth.validation.ts
@@ -0,0 +1,15 @@
+import z, { ZodType } from "zod";
+
+export class AuthValidation {
+  static readonly REGISTER: ZodType = z.object({
+    name: z.string().min(1).max(100),
+    email: z.string().email().min(1).max(100),
+    password: z.string().min(1).max(100),
+  });
+
+  static readonly LOGIN: ZodType = z.object({
+    email: z.string().email().min(1).max(100),
+    password: z.string().min(1).max(100),
+  });
+
+}
