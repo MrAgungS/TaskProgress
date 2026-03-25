fcbace67a90b89ec4468a3e2f39fe939b9a18185
MrAgungS <agung.worksss@gmail.com>
2026-03-25 13:06:00 +0700

    continue tonight

--- backend-nest/src/task/task.service.ts
+++ backend-nest/src/task/task.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { PrismaService } from 'src/common/prisma/prisma.service';
+import { RedisService } from 'src/common/redis/redis.service';
+import { Logger } from 'winston';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  constructor(
+    @Inject(WINSTON_MODULE_PROVIDER) private logger: Logger,
+    private prismaService: PrismaService,
+    private redisService: RedisService,
+  ) {}
+
+  async getTasks() {}
+  async getTasksId() {}
+  async createTask() {}
+  async updateTask() {}
+  async deleteTask() {}
+}
