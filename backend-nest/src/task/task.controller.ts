4d0fc224f2adecfd0a4c8bf7ed81944ea6883d48
MrAgungS <agung.worksss@gmail.com>
2026-03-30 12:05:08 +0700

    testing postman

--- backend-nest/src/task/task.controller.ts
+++ backend-nest/src/task/task.controller.ts
@@ -4,6 +4,7 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
   Request,
@@ -18,33 +19,42 @@ export class TaskController {
   constructor(private taskService: TaskService) {}
 
   @Get('/')
-  getTasks(@Request() req: { user_id: number }) {
-    return this.taskService.getTasks(req.user_id);
+  getTasks(@Request() req: { user: { id: number } }) {
+    return this.taskService.getTasks(req.user.id);
   }
 
   @Get('/:id')
-  getTasksById(@Param('id') id: number, @Request() req: { user_id: number }) {
-    return this.taskService.getTaskById(id, req.user_id);
+  getTasksById(
+    @Param('id', ParseIntPipe) id: number,
+    @Request() req: { user: { id: number } },
+  ) {
+    return this.taskService.getTaskById(id, req.user.id);
   }
 
   @Post('/')
-  createTask(@Body() dto: CreateTaskDto, @Request() req: { user_id: number }) {
-    return this.taskService.createTask(dto, req.user_id);
+  createTask(
+    @Body() dto: CreateTaskDto,
+    @Request() req: { user: { id: number } },
+  ) {
+    return this.taskService.createTask(dto, req.user.id);
   }
 
   @Patch('/:id')
   @Roles(Role.user)
   updateTask(
-    @Param('id') id: number,
+    @Param('id', ParseIntPipe) id: number,
     @Body() dto: UpdateTaskDto,
-    @Request() req: { user_id: number },
+    @Request() req: { user: { id: number } },
   ) {
-    return this.taskService.updateTask(id, dto, req.user_id);
+    return this.taskService.updateTask(id, dto, req.user.id);
   }
 
   @Delete('/:id')
   @Roles(Role.user)
-  deleteTask(@Param('id') id: number, @Request() req: { user_id: number }) {
-    return this.taskService.deleteTask(id, req.user_id);
+  deleteTask(
+    @Param('id', ParseIntPipe) id: number,
+    @Request() req: { user: { id: number } },
+  ) {
+    return this.taskService.deleteTask(id, req.user.id);
   }
 }
